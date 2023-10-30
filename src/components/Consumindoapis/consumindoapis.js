5c93ede329c38e735e7898d2b8e6084a30d7d7d8
pedroalma <pedroalma554@gmail.com>
2023-10-30 08:46:40 -0300

    novo

--- src/components/Consumindoapis/consumindoapis.js
+++ src/components/Consumindoapis/consumindoapis.js
@@ -1,25 +1,41 @@
 import { useState } from "react";
 import axios from 'axios';
 function Consumindoapis(){
-    const[buscaUser,setBuscaUser] = useState();
-    const api = 'https://api.postmon.com.br/v1/cep/' + buscaUser;
+    const[busCep,setBusCep] = useState();
+    const[ data , setData] = useState([]);
+    const api = `https://viacep.com.br/ws/${busCep}/json`; 
       function buscapis(e){
         e.preventDefault();
         axios.get(api)
         .then(Response => {
-            console.log(Response.data);
+            if(Response.data.erro == true){
+                setData([])
+            }else{
+                setData(Response.data)
+            }
         }).catch(error => {
             console.log(error);
         })
       }
     return(
+        <>
         <form>
-            <label>Pesquisar usuario:</label>
+            <label>Pesquisar cep:</label>
             <br/>
-            <input type="text" value={buscaUser} onChange={(e) => setBuscaUser(e.target.value)}/>
+            <input type="text" value={busCep} onChange={(e) => setBusCep(e.target.value)}/>
             <br/>
             <button onClick={buscapis}>Pesquisar</button>
         </form>
+        <p>
+        { data.length === 0 ? 'digite um cep para buscar'
+        :
+        `${data.cep} 
+                   ${data.bairro} 
+                                 ${data.localidade} 
+                                                   ${data.logradouro}
+            `}
+        </p>
+        </>
     )
 }
 export default Consumindoapis;
